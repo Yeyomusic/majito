722532f1484be1e8d3228490fe6528221a784ce3
Yeyomusic <116053352+Yeyomusic@users.noreply.github.com>
2022-10-17 23:51:41 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞Te voy a chupar esas chichis💞');
+    alert('🥰Y le voy a dar beshitos por todo el cuerpo🥰');
+    location.href = 'https://youtu.be/J8NrkPLDi_k&t=17s';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
